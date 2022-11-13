c79ff7d5bb5d9506c8b1823a42d5bfec5afa9d10
Zitrul <63922553+Zitrul@users.noreply.github.com>
2022-11-13 21:09:05 +0300

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,25 @@
+public class Main {
+    private double a;
+    private double b;
+
+    public Main(double a, double b){
+        this.a = a;
+        this.b = b;
+
+
+    }
+    public double sum() {
+        return a+b;
+    }
+    public double proisv() {
+        return a*b;
+    }
+    public double delen() {
+        return a/b;
+    }
+    public double rasn() {
+        return a/b;
+    }
+
+
+}
