f6a1901a927c59368bd3a3790420afa7d3ecfc5c
Zitrul <63922553+Zitrul@users.noreply.github.com>
2022-11-16 20:22:58 +0300

    Update Amount.java

--- Amount.java
+++ Amount.java
@@ -1,6 +1,4 @@
-//1
 Amount amount1 = new Amount();
-//4
 float x;
 float y;
 public GraphPoint(float x1, float y1){
@@ -8,14 +6,12 @@ public GraphPoint(float x1, float y1){
     Point point = new Point(x1,y1);
         
 }
-//2
 public Time(int time){
     this.hour = time/60;
     this.min = time%60;
 }
-//6
 public GroundhogDate(){
      this.month = "Feb";
      this.day = 2;
 }
-//
+
