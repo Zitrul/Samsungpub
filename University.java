343b8fec664d110d15ac67be3ed4b3c365d6c2e0
Zitrul <63922553+Zitrul@users.noreply.github.com>
2022-11-29 20:25:10 +0300

    Create University.java

---
+++ University.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class University implements Printable{
+    String adress;
+    ArrayList<Student> students = new ArrayList<Student>();
+    ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+    public University(ArrayList<Student> s,ArrayList<Teacher> t, String adress){
+        this.students = s;
+        this.teachers = t;
+        this.adress = adress;
+    }
+    public void printInfoToConsole(){
+        System.out.println("+++++++++++++++++++++++++++++++\n" +
+                "Приветсвуем вас в университете "+this.adress+"\n"+
+                "Вот информация о наших лудших ученикаках\n"
+
+        );
+        for(Student student : students){
+            student.printInfoToConsole();
+        }
+        System.out.println("+++++++++++++++++++++++++++++++\n" +
+                "Вот информация о наших лудших учитилях\n"
+
+        );
+        for(Teacher teacher : teachers){
+            teacher.printInfoToConsole();
+        }
+
+
+    }
+}
