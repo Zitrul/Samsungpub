d4ed3176e1f900ea144860569334ec48fcee191e
Zitrul <63922553+Zitrul@users.noreply.github.com>
2022-11-29 20:26:11 +0300

    Create DomoService.java

---
+++ DomoService.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class DomoService {
+    University unever;
+    String adress;
+    public DomoService(String adress){
+        this.adress = adress;
+
+
+    }
+    public void demo(){
+        Student s = new Student("Alex",80800,"NN","Lexa","IT");
+        Teacher t = new Teacher("N",80790,"S","P","IT");
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(s);
+        ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+        teachers.add(t);
+        University unever = new University( students,teachers,this.adress);
+
+        unever.printInfoToConsole();
+
+    }
+}
