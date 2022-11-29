41e40e60833c7ec50355898e60d5acd234fb16e7
Zitrul <63922553+Zitrul@users.noreply.github.com>
2022-11-29 20:37:45 +0300

    Update DomoService.java

--- DomoService.java
+++ DomoService.java
@@ -4,11 +4,16 @@ public class DomoService {
     String adress = "House";
     public void demo(){
         Student s = new Student("Alex",80800,"NN","Lexa","IT");
+        Student s1 = new Student("ADAKSD",808340,"DSFPDSFKLSDJF","DSFSDF","Algebra");
         Teacher t = new Teacher("N",80790,"S","P","IT");
+        Teacher t1 = new Teacher("G",874554,"KJKFD","OFDGKDFG","Algebra");
         ArrayList<Student> students = new ArrayList<Student>();
         students.add(s);
+        students.add(s1);
         ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+
         teachers.add(t);
+        teachers.add(t1);
         unever = new University( students,teachers,this.adress);
 
         unever.printInfoToConsole();
