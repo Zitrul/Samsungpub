bbd1fb3f0e599d0eefdaff5f3217d8ff5a8bdcda
Zitrul <63922553+Zitrul@users.noreply.github.com>
2022-12-01 20:37:39 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,24 +1,35 @@
 public class Student extends User implements Printable{
-    String group;
+    private String group;
     public void sayHello(){
 
         System.out.printf("Привет! Я студен, мое имя", super.name);
     }
-    public Student(String name, int phone,String Secondrname,String patronymic,String group){
-
-        super(name, phone,Secondrname,patronymic);
+    public Student(String name, int phone,String secondrname,String patronymic,String group){
+        super.phone = phone;
+        super.name = name;
+        super.secondrname = secondrname;
+        super.patronymic = patronymic;
         this.group = group;
     }
-    public Student(String name,String Secondrname,String patronymic,String group){
-        super(name,Secondrname,patronymic);
+    public Student(String name,String secondrname,String patronymic,String group){
+        super.name = name;
+        super.secondrname = secondrname;
+        super.patronymic = patronymic;
+
+
         this.group = group;
     }
     public Student(Student student){
-        super(student);
+        super.name = student.name;
+        super.secondrname = student.secondrname;
+        super.patronymic = student.patronymic;
+
+
+        this.group = student.group;
     }
     public void printInfoToConsole(){
         System.out.println("================================\n" +
-                "Привет, Я ученик!\n"+"Меня зовут "+super.Secondrname +" "+super.name+" "+super.patronymic+"\nЯ учусь в группе"+" "+this.group);
+                "Привет, Я ученик!\n"+"Меня зовут "+super.secondrname +" "+super.name+" "+super.patronymic+"\nЯ учусь в группе"+" "+this.group);
     }
 
 
