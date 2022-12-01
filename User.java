3bf58ec5c00335d6f289c0031fc8d79ebe71074c
Zitrul <63922553+Zitrul@users.noreply.github.com>
2022-12-01 20:39:17 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,30 +1,30 @@
 public abstract class User {
-    int phone;
-    String name;
-    String Secondrname;
-    String patronymic;
+    protected int phone;
+    protected String name;
+    protected String secondrname;
+    protected String patronymic;
     public abstract void sayHello();
-    public  User(String name, int phone,String Secondrname,String patronymic){
-        this.name = name;
-        this.phone = phone;
-        this.Secondrname = Secondrname;
-        this.patronymic = patronymic;
-    }
-    public  User(String name,String Secondrname,String patronymic){
-        this.name = name;
-        this.Secondrname = Secondrname;
-        this.patronymic = patronymic;
-    }
-    public  User(Student student){
-        this.name = student.name;
-        this.Secondrname = student.Secondrname;
-        this.patronymic = student.patronymic;
-        this.phone = student.phone;
-    }
-    public  User(Teacher teacher){
-        this.name = teacher.name;
-        this.Secondrname = teacher.Secondrname;
-        this.patronymic = teacher.patronymic;
-        this.phone = teacher.phone;
-    }
+//    public  User(String name, int phone,String Secondrname,String patronymic){
+//        this.name = name;
+//        this.phone = phone;
+//        this.secondrname = Secondrname;
+//        this.patronymic = patronymic;
+//    }
+//    public  User(String name,String Secondrname,String patronymic){
+//        this.name = name;
+//        this.secondrname = Secondrname;
+//        this.patronymic = patronymic;
+//    }
+//    public  User(Student student){
+//        this.name = student.name;
+//        this.secondrname = student.secondrname;
+//        this.patronymic = student.patronymic;
+//        this.phone = student.phone;
+//    }
+//    public  User(Teacher teacher){
+//        this.name = teacher.name;
+//        this.secondrname = teacher.secondrname;
+//        this.patronymic = teacher.patronymic;
+//        this.phone = teacher.phone;
+//    }
 }
